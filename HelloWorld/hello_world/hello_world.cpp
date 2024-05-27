d0d29df7249c89929004131d0ec9ce38d3f76c2e
lugy <lugengyou@github.com>
2024-05-27 10:40:41 +0800

    添加 Essential 和 Primer 源码

--- HelloWorld/hello_world/hello_world.cpp
+++ HelloWorld/hello_world/hello_world.cpp
@@ -4,16 +4,20 @@
  * @Author: Lugy
  * @Date: 2023-08-27 22:38:15
  * @LastEditors: Andy
- * @LastEditTime: 2023-08-27 23:23:20
+ * @LastEditTime: 2024-05-18 10:50:15
  */
 
 #include <iostream>
 
+void print_hello_world(void)
+{
+    std::cout << "Hello world." << std::endl;
+}
 
 int main(void)
 {
 
-    std::cout << "Hello world." << std::endl;
+    print_hello_world();
 
     return 0;
 }
